23bca7bc8f6837d4282d9d6a5e22b72f00478740
Abhishek <abhishekmahalle96@gmail.com>
2023-04-07 21:01:57 +0530

    Adding the Aqua Project

---
+++ Aqua Project/aquaproject/src/main/java/com/aqua/aquaproject/AquaprojectApplication.java
@@ -0,0 +1,13 @@
+package com.aqua.aquaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquaprojectApplication.class, args);
+	}
+
+}
